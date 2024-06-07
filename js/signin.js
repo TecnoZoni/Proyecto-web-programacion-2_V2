382fb91e39ce7d77d8261b0bb864cf1963d75d1b
Admin <agustinminzoni@gmail.com>
2024-06-07 19:55:44 -0300

    cambios en el login

--- js/signin.js
+++ js/signin.js
@@ -8,15 +8,11 @@ const buttonForm = document.getElementById('button-form');
 //autenticación
 authForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    /* if (formTitle.textContent === 'Regístrate') {
-        const password = document.getElementById('password').value;
-        const confirmPassword = document.getElementById('confirm-password').value;
-        if (password !== confirmPassword) {
-            alert('¡Las contraseñas no coinciden!');
-            return;
-        }
-        alert('¡Registro exitoso!'); */
-    const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    alert('¡Inicio exitoso!');
+    const confirmPassword = document.getElementById('confirm-password').value;
+    if (password !== confirmPassword) {
+        alert('¡Las contraseñas no coinciden!');
+        return;
+    }
+    alert('¡Registro exitoso!');
 });
